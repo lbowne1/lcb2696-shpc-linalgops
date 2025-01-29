bfc29a52be6ee33a76d2a8521e815d36759d63d3
Lauren Bowne <lbowne@utexas.edu>
2025-01-29 00:14:23 -0600

    correct gemm

--- src/shpc_gemm.c
+++ src/shpc_gemm.c
@@ -5,22 +5,23 @@ void shpc_dgemm( int m, int n, int k,
                     double *B, int rsB, int csB,                                
                     double *C, int rsC, int csC )
 {
-    double* A_ptr = A;
-    double* B_ptr = B;
-    double* C_ptr = C;
+    double* A_ptr;
+    double* B_ptr;
+    double* C_ptr;
 
     for (int i = 0; i < m; i++) {
-        A_ptr = A + (i * rsA);
         for (int j = 0; j < n; j++) {
-            B_ptr = B + (j * csB);
-            int sum = 0;
+            double sum = 0;
+            double* A_ptr = A + (i * rsA);
+            double* B_ptr = B + (j * csB);
+
             for (int p = 0; p < k; p++) {
-                sum += (*A_ptr * *B_ptr);
+                sum += (*A_ptr) * (*B_ptr);
                 A_ptr += csA;
                 B_ptr += rsB;
             }
-            C_ptr = (C + i * rsC + j * csC);
-            *C_ptr += sum;
+
+            C[(i * rsC) + (j * csC)] = sum; 
         }
     } 
 
